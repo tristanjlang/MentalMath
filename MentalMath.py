063709ee62342549e514fedd31f20ea82592bc30
tristanjlang <tristan@alum.mit.edu>
2014-02-26 15:59:13 -0500

    updated objectives in docstring

---
+++ MentalMath.py
@@ -0,0 +1,15 @@
+import random
+
+'''
+Randomly generate two numbers:
+-50% chance the number is a whole number vs percent with 1 decimal place
+-if the first number is a whole number, the second must be a percent
+
+Prompt user for input:
+-If the result is "quit" then end the loop
+-If the result is correct, tell them
+-Display the total time the user took to work on the problem
+
+Wait 3-5 seconds, then loop back through and get two new numbers
+'''
+
