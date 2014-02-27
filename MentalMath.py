d6672bed4c56a99a888a966803031278741d6e74
tristanjlang <tristan@alum.mit.edu>
2014-02-26 19:00:16 -0500

    completed mental math first pass with average time to solve

--- MentalMath.py
+++ MentalMath.py
@@ -1,9 +1,10 @@
+from decimal import *
 import random
+from timeit import default_timer as timer
 
 '''
 Randomly generate two numbers:
 -50% chance the number is a whole number vs percent with 1 decimal place
--if the first number is a whole number, the second must be a percent
 
 Prompt user for input:
 -If the result is "quit" then end the loop
@@ -13,3 +14,29 @@ Prompt user for input:
 Wait 3-5 seconds, then loop back through and get two new numbers
 '''
 
+userinput = ''
+numtype = ('whole', 'percent')
+times = []
+
+while True:
+    choice = random.choice(numtype)
+    n1 = Decimal(random.randint(0, 1000)) if choice == 'whole' else Decimal(random.randint(0, 100)) / Decimal(100)
+    n2 = Decimal(random.randint(0, 100)) / Decimal(100)
+    print('Solve: ' + str(n1) + ' x ' + str(n2))
+    start = timer()
+
+    userinput = input('Input: ')
+    if userinput.lower() == 'quit': break
+
+    try:
+        userinput = Decimal(userinput)
+        end = timer()
+        elapsed = round(end - start, 1)
+        if userinput == n1 * n2: print('CORRECT')
+        else: print('INCORRECT...' + str(n1 * n2))
+        print('Time to solve: ' + str(elapsed) + ' seconds\n')
+        times.append(elapsed)
+    except:
+        print('Improper entry, next problem...\n')
+
+print('Average time to solve: ' + str(round(sum(times) / len(times), 1)) + ' seconds')
